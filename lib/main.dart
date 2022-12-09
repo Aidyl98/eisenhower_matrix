9b2ff98cf18175078ba2a4d1b1932c8c7df97865
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-09 16:05:10 -0500

    main: + statusBarColor: Colors.transparent

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
