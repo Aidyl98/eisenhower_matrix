b26d3df389faaeb92359bee031ef271a7e6107ad
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-09 15:59:38 -0500

    constants: + statusbarHeight & appBarHeight

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 double statusbarHeight(BuildContext context) =>
-    MediaQuery.of(context).padding.top;
+    MediaQuery.of(context).padding.top; 
 
 double appBarHeight(BuildContext context) => statusbarHeight(context) + 50.0;
